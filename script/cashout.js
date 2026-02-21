f0a123ccc810f9834f17a2be5f8621ab113fb8a2
Mehedi <mehedi.interface@gmail.com>
2026-02-21 11:10:17 +0600

    24-9 Implement Cashout Functionality

--- script/cashout.js
+++ script/cashout.js
@@ -1,9 +1,45 @@
 document.getElementById("cashout-btn").addEventListener("click", function() {
     // 1 - get the agent number & validate 
+    const cashoutNumberInput = document.getElementById("cashout-number"); 
+    const cashoutNumber = cashoutNumberInput.value; 
+    console.log(cashoutNumber); 
+    if (cashoutNumber.length != 11) {
+        alert("Invalid Agent Number"); 
+        return; 
+    }
+
     // 2 - get the amount, validate, convert to number 
-    // 3 - get current balance, validate, convert to number
+    const cashoutAmountInput = document.getElementById("cashout-amount"); 
+    const cashoutAmount = cashoutAmountInput.value; 
+    console.log(cashoutAmount);
+
+    // 3 - get the current balance, validate, convert to number
+    const balanceElement = document.getElementById("balance"); 
+    const balance = balanceElement.innerText; 
+    console.log(balance); 
+
     // 4 - calculate new balance 
+    const newBalance = Number(balance) - Number(cashoutAmount); 
+    console.log("New Balance: ", newBalance); 
+
+    if(newBalance < 0) {
+        alert("Invalid Amount"); 
+        return; 
+    }
+    console.log("New Balance: ", newBalance)
+
     // 5 - get the pin and verify 
+    const cashoutPinInput = document.getElementById("cashout-pin"); 
+    const pin = cashoutPinInput.value; 
+    if (pin === "1234") {
         // true:: show an alert > set Balance 
-        // false:: show an error > return 
+        alert("Cashout Successful")
+        console.log("New Balance: ", newBalance); 
+        balanceElement.innerText = newBalance; 
+    } else {
+         // false:: show an error > return 
+         alert("Invalid pin"); 
+    }
+        
+       
 })
